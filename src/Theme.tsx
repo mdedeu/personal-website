eec9b51f1005b6d759008ef33a96620b7fbe0aca
mdedeu <mdedeu@itba.edu.ar>
2022-07-24 22:47:22 -0300

    Dark Mode default

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,8 +1,13 @@
 import {extendTheme} from "@chakra-ui/react";
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+}
+
 const theme= extendTheme({
       fonts: {
          heading: `'Inter', sans-serif`,
          body: `'Roboto', sans-serif`,
-      }
+      }, config
 })
 export default theme
--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import {Flex, Heading, Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel} from "@chakra-ui/react";
+import {
+    Flex,
+    Heading,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    Box,
+    AccordionIcon,
+    AccordionPanel,
+    WrapItem, Wrap
+} from "@chakra-ui/react";
 import EducationCard from "./EducationCard";
 
 const Education = () => {
     return (
         <Flex direction="column" justify="center" align="center" id="education" gap={8}>
             <Heading textColor="green.400">Education</Heading>
-            <Flex justify="center" align="center">
-                <EducationCard institution="Instituto Tecnológico de Buenos Aires" image="/itba.avif" title="Software Engineering" year="2018 - 2024"/>
-                <EducationCard institution="Google" image="/google.avif" title="Google Cloud Essentials" year="2022"/>
-                <EducationCard institution="University of Buffalo" image="/suny.avif" title="Blockchain Basics" year="2022"/>
-            </Flex>
+            <Wrap justify="center" align="center">
+                <WrapItem>
+                    <EducationCard institution="Instituto Tecnológico de Buenos Aires" image="/itba.avif" title="Software Engineering" year="2018 - 2024"/>
+                </WrapItem>
+                <WrapItem>
+                    <EducationCard institution="Google" image="/google.avif" title="Google Cloud Essentials" year="2022"/>
+                </WrapItem>
+                <WrapItem>
+                    <EducationCard institution="University of Buffalo" image="/suny.avif" title="Blockchain Basics" year="2022"/>
+                </WrapItem>
+            </Wrap>
         </Flex>
     );
 };
