27d938db9d5cde1c7a3d0f7985810fe7f1e86ee4
mdedeu <mdedeu@itba.edu.ar>
2022-07-24 22:59:24 -0300

    Dark default

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
+    <ColorModeScript initialColorMode='dark' />
     <App />
   </React.StrictMode>,
 )
